9dafa8ca2858b520fb7b48e1c32a1e4939076dac
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2017-08-18 09:58:27 +0900

    正解生成アルゴリズムの実装。

--- HitAndBlow/src/main/Main.java
+++ HitAndBlow/src/main/Main.java
@@ -4,12 +4,40 @@ import java.io.BufferedReader;
 import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
+
+	private static final boolean DEBUG_MODE = true;
+	private static final long DEBUG_SEED = 1234;
+
+	private static final int DIGITS = 4;
+
+	private static final Random seed;
+
+	static {
+		if (DEBUG_MODE) {
+			seed = new Random(DEBUG_SEED);
+		} else {
+			seed = new Random(System.currentTimeMillis());
+		}
+
+		// 初回の乱数は捨てる
+		seed.nextInt();
+	}
 
 	public static void main(String[] args) {
 		CommandReader reader = new CommandReader();
 		String line = null;
+
+		final String expected = createAnswer();
+		if (DEBUG_MODE) {
+			System.out.println("DEBUG: 正解： " + expected);
+		}
 
 		System.out.println("以下のコマンドを入力してください。");
 		System.out.println("ゲームを終了する： exit");
@@ -24,6 +52,18 @@ public class Main {
 		}
 
 		System.out.println("お疲れ様でした。");
+	}
+
+	private static String createAnswer() {
+		List<String> numbers = IntStream.rangeClosed(0, 9).mapToObj(Integer::toString).collect(Collectors.toList());
+		List<String> list = new ArrayList<>();
+
+		for (int i = 0; i < DIGITS; i++) {
+			int index = seed.nextInt(numbers.size());
+			list.add(numbers.remove(index));
+		}
+
+		return String.join("", list);
 	}
 
 }
