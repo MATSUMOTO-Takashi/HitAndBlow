1784967f2de27e1f166727d4c7d0675c4b5fc9ee
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2017-08-18 11:07:32 +0900

    チェックロジックを実装。

--- HitAndBlow/src/main/Main.java
+++ HitAndBlow/src/main/Main.java
@@ -5,6 +5,8 @@ import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -12,32 +14,15 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-	private static final boolean DEBUG_MODE = true;
 	private static final long DEBUG_SEED = 1234;
-
 	private static final int DIGITS = 4;
 
-	private static final Random seed;
-
-	static {
-		if (DEBUG_MODE) {
-			seed = new Random(DEBUG_SEED);
-		} else {
-			seed = new Random(System.currentTimeMillis());
-		}
-
-		// 初回の乱数は捨てる
-		seed.nextInt();
-	}
+	private static boolean DEBUG_MODE = false;
+	private static Random GEN;
 
 	public static void main(String[] args) {
 		CommandReader reader = new CommandReader();
 		String line = null;
-
-		final String expected = createAnswer();
-		if (DEBUG_MODE) {
-			System.out.println("DEBUG: 正解： " + expected);
-		}
 
 		System.out.println("以下のコマンドを入力してください。");
 		System.out.println("ゲームを終了する： exit");
@@ -45,13 +30,57 @@ public class Main {
 
 		line = reader.readLine();
 
+		if (line.equals("DEBUG")) {
+			DEBUG_MODE = true;
+		}
+		init();
+
+		final String expected = createAnswer();
+		if (DEBUG_MODE) {
+			System.out.println("DEBUG: 正解： " + expected);
+		}
+
 		while (!(line == null || line.equals("exit"))) {
-			System.out.println("入力してください。");
+			System.out.println("質問する数値を入力してください。");
 			line = reader.readLine();
 
+			if (line.length() != DIGITS) {
+				System.out.println("入力桁数が" + DIGITS + "ではありません。");
+				continue;
+			}
+
+			if (!line.matches("[0-9]+")) {
+				System.out.println("数値以外が含まれています。");
+				continue;
+			}
+
+			if (new HashSet<>(Arrays.asList(line.split(""))).size() != DIGITS) {
+				System.out.println("重複した数値が含まれています。");
+				continue;
+			}
+
+			int[] result = checkTheAnswer(expected, line);
+			System.out.println("入力値 " + line + " は、" + result[0] + "ヒット、" + result[1] + "ブロウです。");
+
+			// ゲームクリア
+			if (result[0] == DIGITS) {
+				System.out.println("正解です！");
+				break;
+			}
 		}
 
 		System.out.println("お疲れ様でした。");
+	}
+
+	private static void init() {
+		if (DEBUG_MODE) {
+			GEN = new Random(DEBUG_SEED);
+		} else {
+			GEN = new Random(System.currentTimeMillis());
+		}
+
+		// 初回の乱数は捨てる
+		GEN.nextInt();
 	}
 
 	private static String createAnswer() {
@@ -59,11 +88,42 @@ public class Main {
 		List<String> list = new ArrayList<>();
 
 		for (int i = 0; i < DIGITS; i++) {
-			int index = seed.nextInt(numbers.size());
+			int index = GEN.nextInt(numbers.size());
 			list.add(numbers.remove(index));
 		}
 
 		return String.join("", list);
+	}
+
+	private static int[] checkTheAnswer(String expected, String actual) {
+		if (expected.length() != actual.length()) {
+			throw new IllegalArgumentException("桁数が異なります。: expected: " + expected.length() + ", actual: " + actual.length());
+		}
+
+		char[] splitExpected = expected.toCharArray();
+		char[] splitActual = actual.toCharArray();
+
+		// [0]=Hit, [1]=Blow
+		int[] result = new int[2];
+
+		for (int i = 0; i < splitExpected.length; i++) {
+			if (splitExpected[i] == splitActual[i]) {
+				result[0]++;
+			}
+		}
+
+		for (int i = 0; i < splitExpected.length; i++) {
+			for (int j = 0; j < splitActual.length; j++) {
+				if (i == j) {
+					continue;
+				}
+				if (splitExpected[i] == splitActual[j]) {
+					result[1]++;
+				}
+			}
+		}
+
+		return result;
 	}
 
 }
